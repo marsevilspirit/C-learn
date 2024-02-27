372f8bca6bb7cee630ed92a0c2a99b753715c3ec
marsevilspirit <a2261214089@qq.com>
2024-02-27 19:46:35 +0800

    learn c++

---
+++ chapter10/find_if.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <string_view>
+
+bool containNut(std::string_view str)
+{
+    return (str.find("nut") != std::string_view::npos);
+}
+
+int main(void)
+{
+    std::array<std::string_view, 4> arr{ "apple", "banana", "walnut", "lemon" };
+
+    auto found{ std::find_if(arr.begin(), arr.end(), containNut)};
+
+    if(found == arr.end())
+    {
+        std::cout << "No nuts\n";
+    }
+    else 
+    {
+        std::cout << "Found " << *found << '\n'; 
+    }
+
+    return 0;
+}
